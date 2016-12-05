293fd5beeca46d599845258cec0fa24b693081bc
aranga-au <aranga.nanayakkara@gmail.com>
2016-12-05 22:35:16 +1100

    security

--- app/security/secure.directive.ts
+++ app/security/secure.directive.ts
@@ -1,15 +1,21 @@
 import { Directive ,ElementRef , Renderer,  AfterViewInit} from '@angular/core';
 import { Router,RouterModule,RouterLinkWithHref } from '@angular/router';
 
+import { AuthService } from './auth.service';
+import { IRights } from './rights';
+
 @Directive({
     selector:'[appSecure]'
 })
 export class SecureDirective implements  AfterViewInit{
-    constructor(private elementRef:ElementRef,private router:Router,private routerLinke :RouterLinkWithHref,private render:Renderer){
-
+    constructor(private elementRef:ElementRef,private router:Router,private routerLinke :RouterLinkWithHref,private authService:AuthService){
+        this.authService.rightsUpdateEvent$.subscribe((r:IRights) => this.onRightsUpdate(r))
     }
     ngAfterViewInit(){
         console.log(this.routerLinke);
          console.log(this.elementRef);
+    }
+    onRightsUpdate(rights :IRights):void{
+
     }
 }
